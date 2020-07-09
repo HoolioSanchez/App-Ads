3e50722257dc76a7e2484c91c4b1004a4512b39b
HoolioSanchez <julio.c.sanchez.pro@gmail.com>
2020-07-09 08:38:35 -0700

    Helper class created

--- app-ads.py
+++ app-ads.py
@@ -2,13 +2,13 @@
 import requests 
 import pandas as pd
 from io import StringIO
+from currentAppAds import unity_app_ads
 
 class AppAds:
     
     def getAppAds(self, url):
         r = requests.request("GET", url)
-
-        return self.getDataFrame(r.text)
+        return self.getDataframe(r.text)
 
     def getDataframe(self, txt):
         return pd.read_csv(self.addStringDelimiator(txt), sep = ';', header=None, usecols=[0])
@@ -17,13 +17,10 @@ class AppAds:
         txt.replace("\n", ";")
         return StringIO(txt)
 
+    def getMissingAppsAds(self, standardList, link):
+        df = self.getAppAds(link)
+        df1 = self.getDataframe(standardList)
 
-#%%
-ads = AppAds()
-link = "https://jamcity.com/app-ads.txt"
+        final = df1[~df1.apply(tuple,1).isin(df.apply(tuple,1))]
 
-data = AppAds.getAppAds(link)
-
-data.head()
-
-# %%
+        return final
